2d0bfe424f388f14e025490a9e10c887de9b2524
bellandry <bellandry.work@gmail.com>
2026-06-07 13:36:51 +0100

    update: move to src directory

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const pool = require("./config/db");
-const redisConnection = require("./config/redis");
-const loggerMiddleware = require("./middlewares/logger");
-const rateLimiter = require("./middlewares/rateLimiter");
+const pool = require("./src/config/db");
+const redisConnection = require("./src/config/redis");
+const loggerMiddleware = require("./src/middlewares/logger");
+const rateLimiter = require("./src/middlewares/rateLimiter");
 const { Queue } = require("bullmq");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const TicketRepository = require("./repositories/ticket.repository");
-const TicketService = require("./services/ticket.service");
-const TicketController = require("./controllers/ticket.controller");
+const TicketRepository = require("./src/repositories/ticket.repository");
+const TicketService = require("./src/services/ticket.service");
+const TicketController = require("./src/controllers/ticket.controller");
 
 const app = express();
 const PORT = process.env.PORT;
